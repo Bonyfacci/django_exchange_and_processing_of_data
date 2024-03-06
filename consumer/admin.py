38f0f74ba304cf0babd2f59f2f26928b2f619c5c
Сергей Платонов <platonovpochta@gmail.com>
2024-03-06 17:22:37 +0100

    app Consumer ✅ add models ✅ add serializers ✅ add view (api) ✅ add url

--- consumer/admin.py
+++ consumer/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from consumer.models import Place, Category, Purchase, CategoryAnalytics, OverallAnalytics
+
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(Purchase)
+class PurchaseAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(CategoryAnalytics)
+class CategoryAnalyticsAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(OverallAnalytics)
+class OverallAnalyticsAdmin(admin.ModelAdmin):
+    ...
